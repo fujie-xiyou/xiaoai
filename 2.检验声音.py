380cff21764b9c698b604ca41fc286de6a96daf9
fujie <fujie@xiyoulinux.org>
2020-05-26 17:11:37 +0800

    ca

--- 2.检验声音.py
+++
@@ -1,9 +0,0 @@
-import sys
-from tools.req import asr_audio
-
-if __name__ == '__main__':
-    args = []
-    for i in range(1, len(sys.argv)):
-        args.append(int(sys.argv[i]))
-    asr_audio(*args)
-
