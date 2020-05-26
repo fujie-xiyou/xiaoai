c549f55708618e1cbeffa505def07014dd16e5b2
fujie <fujie@xiyoulinux.org>
2020-05-26 16:27:36 +0800

    Windows

--- 3.上传声音.py
+++
@@ -1,4 +0,0 @@
-from tools.req import post_record
-
-if __name__ == '__main__':
-    post_record()
--- tools/m4a2wav.py
+++
@@ -1,9 +0,0 @@
-import os
-import config
-
-"""
-如果你的音频文件是m4a可以用这个文件处理
-"""
-
-for i in range(1, 21):
-    os.system("ffmpeg -i %s.m4a -ac 1 -ar 16000 -acodec pcm_s16le -f wav %s.wav" % (i, i))
